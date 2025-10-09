ec28da3cb1f2a6ee9cae0b7a23b08f8ee268b2af
Renn <chrizraine.pena@gmail.com>
2025-10-09 17:36:55 +0800

    Midterm Exam

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
   has_many :sections, through: :classlists, dependent: :destroy
   has_many :guardians, through: :relationship, dependent: :destroy 
   has_many :relationships, dependent: :destroy
+
+  def refresh_student_count!
+    update!(number_of_guardians: relationships.count)
+  end
 end
